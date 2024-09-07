4728e85af6d41941ff2fdef9abb5bf85ba9649dc
jrbigmon <juniorterriaga0@gmail.com>
2024-09-06 22:10:19 -0300

    feat: seeding to orders

--- src/main/java/com/jrbigmon/course/config/TestConfig.java
+++ src/main/java/com/jrbigmon/course/config/TestConfig.java
@@ -5,25 +5,43 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.jrbigmon.course.entities.Order;
 import com.jrbigmon.course.entities.User;
+import com.jrbigmon.course.repositories.IOrderRepository;
 import com.jrbigmon.course.repositories.IUserRepository;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.time.Instant;
 
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
-  
+
   @Autowired
   private IUserRepository userRepository;
+
+  @Autowired
+  private IOrderRepository orderRepository;
 
   @Override
   public void run(String... args) throws Exception {
     List<User> users = new ArrayList<>();
-    
-    users.add(new User("Vagner", "vagner@mail.com", "11321123321", "123456"));
-    users.add(new User("aretha", "aretha@mail.com", "11321123123", "321654"));
+    List<Order> orders = new ArrayList<>();
+
+    User user1 = new User("Vagner", "vagner@mail.com", "11321123321", "123456");
+    User user2 = new User("aretha", "aretha@mail.com", "11321123123", "321654");
+
+    users.add(user1);
+    users.add(user2);
+
+    Order order1 = new Order(Instant.parse("2024-09-06T21:00:00Z"), user1);
+    Order order2 = new Order(Instant.parse("2024-09-07T10:00:00Z"), user2);
+
+    orders.add(order1);
+    orders.add(order2);
 
     this.userRepository.saveAll(users);
+    this.orderRepository.saveAll(orders);
   }
 }
